ad73f9904b9eb80f79b7fb1338eb6946f58d6ae0
Gtoyos <guille.toyos@gmail.com>
2018-06-05 21:44:29 -0300

    lola3

--- core/lol/lol.py
+++ core/lol/lol.py
@@ -387,7 +387,7 @@ class Lol:
             self.lolicons(blueteam[2][1])+"**"+blueteam[2][3].title()+"**\n"+      #La idea es que empiezen en mayuscula pero que el nombre no pierda
             self.lolicons(blueteam[3][1])+"**"+blueteam[3][3].title()+"**\n"+      #las mayusculas originales.
             self.lolicons(blueteam[4][1])+"**"+blueteam[4][3].title()+"**\n", inline=True)
-        embed.add_field(name=" Solo ｜ Flex", value=self.lolicons(blueteam[0][4][0].lower().capitalize())+"**"+blueteam[0][4][1]+"**  "+
+        embed.add_field(name=self.lolicons("Null")+"Solo ｜ Flex", value=self.lolicons(blueteam[0][4][0].lower().capitalize())+"**"+blueteam[0][4][1]+"**  "+
             self.lolicons(blueteam[0][5][0].lower().capitalize())+"**"+blueteam[0][5][1]+"**\n"+
             self.lolicons(blueteam[1][4][0].lower().capitalize())+"**"+blueteam[1][4][1]+"**  "+
             self.lolicons(blueteam[1][5][0].lower().capitalize())+"**"+blueteam[1][5][1]+"**\n"+
@@ -397,11 +397,11 @@ class Lol:
             self.lolicons(blueteam[3][5][0].lower().capitalize())+"**"+blueteam[3][5][1]+"**\n"+
             self.lolicons(blueteam[4][4][0].lower().capitalize())+"**"+blueteam[4][4][1]+"**  "+
             self.lolicons(blueteam[4][5][0].lower().capitalize())+"**"+blueteam[4][5][1]+"**\n", inline=True)
-        embed.add_field(name=self.lolicons("Null")+"Winrate",value=str(blueteam[0][6])+"% *("+"W"+(str(blueteam[0][7]))+", "+"L"+(str(blueteam[0][8]))+")*\n"+
-            str(blueteam[0][6])+"% *("+"W"+(str(blueteam[1][7]))+", "+"L"+(str(blueteam[1][8]))+")*\n"+
-            str(blueteam[0][6])+"% *("+"W"+(str(blueteam[2][7]))+", "+"L"+(str(blueteam[2][8]))+")*\n"+
-            str(blueteam[0][6])+"% *("+"W"+(str(blueteam[3][7]))+", "+"L"+(str(blueteam[3][8]))+")*\n"+
-            str(blueteam[0][6])+"% *("+"W"+(str(blueteam[4][7]))+", "+"L"+(str(blueteam[4][8]))+")*", inline=True)
+        embed.add_field(name=self.lolicons("Null")+"Winrate",value=str(self.lolicons("Null")+blueteam[0][6])+"% *("+"W"+(str(blueteam[0][7]))+", "+"L"+(str(blueteam[0][8]))+")*\n"+
+            self.lolicons("Null")+str(blueteam[1][6])+"% *("+"W"+(str(blueteam[1][7]))+", "+"L"+(str(blueteam[1][8]))+")*\n"+
+            self.lolicons("Null")+str(blueteam[2][6])+"% *("+"W"+(str(blueteam[2][7]))+", "+"L"+(str(blueteam[2][8]))+")*\n"+
+            self.lolicons("Null")+str(blueteam[3][6])+"% *("+"W"+(str(blueteam[3][7]))+", "+"L"+(str(blueteam[3][8]))+")*\n"+
+            self.lolicons("Null")+str(blueteam[4][6])+"% *("+"W"+(str(blueteam[4][7]))+", "+"L"+(str(blueteam[4][8]))+")*", inline=True)
         await ctx.send(embed=embed)
         #embed=discord.Embed(title=maptype+" "+gametype+" "+"Game", description="Average Elo: "+"Solo/Duo:"+" "+avgsololeague+" "+avgsolonum+" , Flex: "+avgflexleague+" "+avgflexnum, color=colorh)
         #embed.set_thumbnail(url=icon)
