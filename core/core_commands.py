f4c2bb590c6482a6880ddf3fb8d20b4e553473a9
Gtoyos <guille.toyos@gmail.com>
2018-06-12 17:33:21 -0300

    minor fixes

--- core/core_commands.py
+++ core/core_commands.py
@@ -553,20 +553,20 @@ class Core(CoreLogic):
             pass
         await ctx.bot.shutdown(restart=True)
 
-  @commands.group(name="set", autohelp=True)
-async def _set(self, ctx):
-    """Changes Red's settings"""
-    if ctx.invoked_subcommand is None:
-        if ctx.guild:
-            admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
-            admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id) or "Not set"
-            mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
-            mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id) or "Not set"
-            prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
-            guild_settings = f"Admin role: {admin_role}\nMod role: {mod_role}\n"
-        else:
-            guild_settings = ""
-            prefixes = None  # This is correct. The below can happen in a guild.
+    @commands.group(name="set", autohelp=True)
+    async def _set(self, ctx):
+        """Changes Red's settings"""
+        if ctx.invoked_subcommand is None:
+            if ctx.guild:
+                admin_role_id = await ctx.bot.db.guild(ctx.guild).admin_role()
+                admin_role = discord.utils.get(ctx.guild.roles, id=admin_role_id) or "Not set"
+                mod_role_id = await ctx.bot.db.guild(ctx.guild).mod_role()
+                mod_role = discord.utils.get(ctx.guild.roles, id=mod_role_id) or "Not set"
+                prefixes = await ctx.bot.db.guild(ctx.guild).prefix()
+                guild_settings = f"Admin role: {admin_role}\nMod role: {mod_role}\n"
+            else:
+                guild_settings = ""
+                prefixes = None  # This is correct. The below can happen in a guild.
         if not prefixes:
             prefixes = await ctx.bot.db.prefix()
         locale = await ctx.bot.db.locale()
